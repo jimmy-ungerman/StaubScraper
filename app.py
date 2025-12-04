caba3601ac4d838d87eecbb0befd307657187b54
Jimmy Ungerman <jimmy.ungerman@gmail.com>
2025-12-04 11:16:04 -0700

    still weird

--- app.py
+++ app.py
@@ -19,12 +19,12 @@ SELENIUM_WAIT = 15  # seconds
 
 def scrape_tanks():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-setuid-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--user-data-dir=/tmp/chrome-data")
 
     chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
     driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
